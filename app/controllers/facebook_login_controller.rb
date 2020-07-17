367fd2995f4c7ae68e48111647bb8e14a781916f
odeane_croney <odeane_croney@yahoo.com>
2020-07-17 12:00:10 +0200

    adding facebokk login

--- app/controllers/facebook_login_controller.rb
+++ app/controllers/facebook_login_controller.rb
@@ -1,5 +1,28 @@
-class FacebookLoginController < ApplicationController
+class FacebookLoginController < DeviseTokenAuth::RegistrationsController
   def create
-    binding.pry
+    if params[:provider] == 'facebook'
+    #create user with facebook credentials
+
+    user = User.from_omniauth(params[:uid], params[:email], params[:provider])
+
+      if user.persisted?
+        sign_in user
+        token = user.create_token
+        user.save 
+        auth_params ={
+          auth_token: token.token,
+          client_id: token.client,
+          uid: params[:uid],
+          expiry: token.expiry 
+        }
+
+        render json: auth_params
+      else
+        render json: {error: 'Authentication with Facebook Failed '}, status: 401
+      end
+    else
+    #create a user in the 'normal way' with email and password
+      super #go through the normal flow in the devise rregistrations controller
+    end
   end
 end
